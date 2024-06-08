8edf3398e3700f7a9c844af0d5742e7fce149a12
Rupam Das <rupam1624@gmail.com>
2024-06-08 15:40:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ document.querySelector("#logg").addEventListener('click',function(){
 document.querySelector(".cross").addEventListener('click',function(){
     document.querySelector(".logbox").classList.remove("active");   
 });
+let a = document.getElementById("user");
+console.log(a);
