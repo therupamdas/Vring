4840c64a2a5eddf6345b4749fd19e1011989a9e9
Rupam Das <rupam1624@gmail.com>
2024-06-08 13:48:19 +0530

    ok

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.querySelector("#logg").addEventListener('click',function(){
-    document.querySelector(".logbox").classList.add("active");
-    
+    document.querySelector(".logbox").classList.add("active");    
+});
+document.querySelector(".cross").addEventListener('click',function(){
+    document.querySelector(".logbox").classList.remove("active");   
 });
