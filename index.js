e6f40705dd9ee99338345b645367b963aaf7e2fa
Rupam Das <rupam1624@gmail.com>
2024-06-10 13:56:29 +0530

    ok

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ document.querySelector("#logg").addEventListener('click',function(){
     document.querySelector(".logbox").classList.add("active");    
 });
 document.querySelector("#logcross").addEventListener('click',function(){
-    document.querySelector(".logbox").classList.remove("active");   
+    document.querySelector(".logbox").classList.remove("active");
+    event.stopImmediatePropagation();  
 });
 
-document.querySelector(".navbar").addEventListener('click',function(){
-    document.querySelector(".alertbox").classList.add("active");    
+document.querySelector("body").addEventListener('click',function(){
+    if (document.querySelector('.logbox').classList.contains('active')==false){
+    document.querySelector(".alertbox").classList.add("active"); 
+    }   
 });
 document.querySelector("#alertcross").addEventListener('click',function(){
-    document.querySelector(".alertbox").classList.remove("active");   
+    document.querySelector(".alertbox").classList.remove("active");
+    event.stopImmediatePropagation();  
 });
--- server.js
+++ server.js
@@ -5,15 +5,9 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.sendFile('C:/Users/rupam/Web Dev/Vring/index.html');
 })
-app.get('/footage', (req, res) => {
-    res.sendFile('C:/Users/rupam/Web Dev/Vring');
-  });
 app.get('/style1.css', (req, res) => {
     res.sendFile('C:/Users/rupam/Web Dev/Vring/style1.css');
   });
-app.get('/editors.html', (req, res) => {
-    res.sendFile('C:/Users/rupam/Web Dev/Vring/editors.html');
-  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
